a85acaaf686fbb061a5207c9f1d382b1ee5da8d2
utkarsh-agrawaal <55744627+utkarsh-agrawaal@users.noreply.github.com>
2019-10-10 14:40:31 +0530

    Delete 10001_squashed_0004_auto_20150617_0902.py

--- 10001_squashed_0004_auto_20150617_0902.py
+++
@@ -1,8 +0,0 @@
-def create_anonymous(apps, schema_editor):
-    User = schema_editor.get_model("users", "User")
-    if not User.objects.filter(display_name='AnonymousUser').exists():
-        User.objects.create(
-            display_name='AnonymousUser',
-            password='',
-            email=''
-        )
