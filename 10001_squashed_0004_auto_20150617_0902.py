bfb73cd4a7f8071dbce6c029a9671afaae7f4ee0
utkarsh-agrawaal <55744627+utkarsh-agrawaal@users.noreply.github.com>
2019-10-10 14:39:28 +0530

    Update 10001_squashed_0004_auto_20150617_0902.py

--- 10001_squashed_0004_auto_20150617_0902.py
+++ 10001_squashed_0004_auto_20150617_0902.py
@@ -1,5 +1,5 @@
 def create_anonymous(apps, schema_editor):
-    User = apps.get_model("users", "User")
+    User = schema_editor.get_model("users", "User")
     if not User.objects.filter(display_name='AnonymousUser').exists():
         User.objects.create(
             display_name='AnonymousUser',
